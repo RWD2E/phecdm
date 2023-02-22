6d4651695ef1dc2840cef2529b337342832f29ab
Song <xsm7f@umsystem.edu>
2023-02-22 09:59:21 -0600

    change hard-coded path into relative path

--- src/py_api-get-call/get-als-cd.py
+++ src/py_api-get-call/get-als-cd.py
@@ -1,20 +1,23 @@
+import os
 import api_get_bioportal as apibp
 import api_get_rxnav as apirxnav
 
+path_to_search_input = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),'res','valueset_autogen')
+
 # diagnostics 
 # apibp.batch_write_vs_json(
-#      'C:/repo/PheCDM/valuesets/valueset_autogen'
+#      path_to_search_input
 #     ,'als-dx_input'
 #     )
 
-# # real world endpoints
-# apibp.batch_write_vs_json(
-#      'C:/repo/PheCDM/valuesets/valueset_autogen'
-#     ,'als-tx_input'
-#     )
+# real world endpoints
+apibp.batch_write_vs_json(
+     path_to_search_input
+    ,'als-tx_input'
+    )
 
 # medications
-sterms = ['riluzole']
-apirxnav.batch_write_ndc_json('C:/repo/PheCDM/valuesets/valueset_autogen',
-                              'als-rx_output',sterms)
+# sterms = ['riluzole']
+# apirxnav.batch_write_ndc_json(path_to_search_input,
+#                               'als-rx_output',sterms)
      
