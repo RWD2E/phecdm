93f9ba4963b83ebfa7de66e90da4995a96c479ba
Song <xsm7f@umsystem.edu>
2023-05-05 15:38:46 -0500

    add comments

--- src/sql-gen_where-clause.py
+++ src/sql-gen_where-clause.py
@@ -6,7 +6,7 @@ import urllib.request as urlreq
 
 def sqlgen_phe_spark(
     src_json_url,      # url where source phenotype mapping can be found, e.g. https://raw.githubusercontent.com/RWD2E/phecdm/main/res/valueset_curated/vs-mmm.json
-    which_phenotype,   # specify codeset for which phenotype
+    which_phenotype,   # specify codeset for which phenotype, currently only allow single string
     cd_field_map,      # actual code field mapping
     cdtype_field_map,  # code type field mapping
     cdtype_value_map,  # code type valueset mapping
@@ -56,31 +56,31 @@ def sqlgen_phe_spark(
     sql_str_master = "(" + ") OR (".join(sql_str_lst) + ")"
     return(sql_str_master)
 
-cd_field_map = {
-    'icd9-cm':'conditioncode.standard.id',
-    'icd10-cm':'conditioncode.standard.id',
-    'hcpcs':'procedurecode.standard.id',
-    'drg':'conditioncode.standard.id'
-}
-cdtype_field_map = {
-    'icd9-cm':'conditioncode.condingSystemId',
-    'icd10-cm':'conditioncode.condingSystemId',
-    'hcpcs':'procedurecode.condingSystemId',
-    'drg':'conditioncode.condingSystemId'
-}
-cdtype_value_map = {
-    'icd9-cm':['2.16.840.1.113883.6.103'],
-    'icd10-cm':['2.16.840.1.113883.6.4'],
-    'hcpcs':['2.16.840.1.113883.6.14'],
-    'drg':['urn:cerner:codingsystem:drg:aprdrg','urn:cerner:codingsystem:drg:apdrg']
-}
+# cd_field_map = {
+#     'icd9-cm':'conditioncode.standard.id',
+#     'icd10-cm':'conditioncode.standard.id',
+#     'hcpcs':'procedurecode.standard.id',
+#     'drg':'conditioncode.standard.id'
+# }
+# cdtype_field_map = {
+#     'icd9-cm':'conditioncode.condingSystemId',
+#     'icd10-cm':'conditioncode.condingSystemId',
+#     'hcpcs':'procedurecode.condingSystemId',
+#     'drg':'conditioncode.condingSystemId'
+# }
+# cdtype_value_map = {
+#     'icd9-cm':['2.16.840.1.113883.6.103'],
+#     'icd10-cm':['2.16.840.1.113883.6.4'],
+#     'hcpcs':['2.16.840.1.113883.6.14'],
+#     'drg':['urn:cerner:codingsystem:drg:aprdrg','urn:cerner:codingsystem:drg:apdrg']
+# }
 
-test = sqlgen_phe_spark(
-    src_json_url = 'https://raw.githubusercontent.com/RWD2E/phecdm/main/res/valueset_curated/vs-mmm.json',
-    which_phenotype = 'delivery-vaginal',
-    cd_field_map = cd_field_map,
-    cdtype_field_map = cdtype_field_map,
-    cdtype_value_map = cdtype_value_map
-)
+# test = sqlgen_phe_spark(
+#     src_json_url = 'https://raw.githubusercontent.com/RWD2E/phecdm/main/res/valueset_curated/vs-mmm.json',
+#     which_phenotype = 'delivery-vaginal',
+#     cd_field_map = cd_field_map,
+#     cdtype_field_map = cdtype_field_map,
+#     cdtype_value_map = cdtype_value_map
+# )
 
-print(test)
+# print(test)
