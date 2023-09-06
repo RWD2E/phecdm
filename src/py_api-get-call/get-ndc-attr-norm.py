80b9940304e43fbe818895d0f23f75e48390b67e
Song <xsm7f@umsystem.edu>
2023-09-06 17:59:34 -0500

    spot test

--- src/py_api-get-call/get-ndc-attr-norm.py
+++ src/py_api-get-call/get-ndc-attr-norm.py
@@ -11,17 +11,19 @@ rxnav_cls = apirxnav.RxNavSearch()
 # print(rxnav_cls.get_rxcui_details("1923432",expand=True))
 # print(rxnav_cls.get_ndc_details("50383023310",expand=True))
 # print(rxnav_cls.get_ndc_details("50383023310",expand=False))
+print(rxnav_cls.get_ndc_details("49100040025",expand=False))
+
 
 # prod run
-src_dir_loc = os.path.dirname(os.path.dirname(os.path.dirname(__file__))) + '/res/valueset_autogen'
-src_file_loc = src_dir_loc + '/pde-ndc-raw.csv'    
-src_vs = pd.read_csv(src_file_loc)
-src_vs['NDC'] = src_vs['NDC'].astype(str).str.zfill(11)
+# src_dir_loc = os.path.dirname(os.path.dirname(os.path.dirname(__file__))) + '/res/valueset_autogen'
+# src_file_loc = src_dir_loc + '/pde-ndc-raw.csv'    
+# src_vs = pd.read_csv(src_file_loc)
+# src_vs['NDC'] = src_vs['NDC'].astype(str).str.zfill(11)
 
-apirxnav.batch_write_ndc_details_json(
-    path_to_save = src_dir_loc, #absolute path,
-    filename_to_save = 'pde-ndc-norm-collapse',
-    sterms = src_vs['NDC'],
-    expand=False,
-    verbose=True
-)
+# apirxnav.batch_write_ndc_details_json(
+#     path_to_save = src_dir_loc, #absolute path,
+#     filename_to_save = 'pde-ndc-norm-collapse',
+#     sterms = src_vs['NDC'],
+#     expand=False,
+#     verbose=True
+# )
