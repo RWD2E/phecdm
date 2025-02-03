3edcba02948ab732b3bd4adf6be5f85a7c96bebf
sxinger <19035729+sxinger@users.noreply.github.com>
2025-02-03 11:01:08 -0600

    clean out testing queries from json_util

--- src/json_util.py
+++ src/json_util.py
@@ -235,47 +235,6 @@ class JsonBlockVS:
         else:
             return "The new json data block is rejected"
 
-# fp = os.path.join(
-#     os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-#     'res','valueset_curated',"vs-als-cde.json"
-# )
-# als_vs = JsonBlockVS(filepath = fp,idstarter='C')
-# print(als_vs.get_existing_id())
-# print(als_vs.generate_new_id())
-# print(als_vs.create_json_data_block())
-# print(als_vs.add_json_block())
-
-
-# fp = os.path.join(
-#     os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-#     'res','valueset_curated',"vs-mmm-cde.json"
-# )
-# mmm_vs = JsonBlockVS(filepath = fp,idstarter='M')
-# print(mmm_vs.add_json_block())
-
-# fp = os.path.join(
-#     os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-#     'res','valueset_curated',"vs-t2dm-cde.json"
-# )
-# t2dm_vs = JsonBlockVS(filepath = fp,idstarter='DM')
-# print(t2dm_vs.get_existing_id())
-# print(t2dm_vs.generate_new_id())
-# print(t2dm_vs.add_json_block())
-
-# fp = os.path.join(
-#     os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-#     'res','valueset_curated',"vs-comorb-OBCMI.json"
-# )
-# comorb_vs = JsonBlockVS(filepath = fp,idstarter='M',idlength=7)
-# print(comorb_vs.generate_new_id())
-# print(comorb_vs.add_json_block())
-
-fp = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-    'res','valueset_curated',"vs-t2dm-cde.json"
-)
-comorb_vs = JsonBlockVS(filepath = fp,idstarter='SDM',idlength=8)
-print(comorb_vs.add_json_block())
 
 
 def json2ref(
@@ -306,10 +265,6 @@ def json2ref(
     expanded_df.to_csv(save_csv_to,index=False)
     return('new valueset saved as ref csv')
 
-# json2ref(
-#     json_url = 'https://raw.githubusercontent.com/RWD2E/phecdm/refs/heads/main/res/valueset_curated/vs-comorb-OBCMI.json',
-#     save_csv_to = 'C:/repos/phecdm/ref/OBCMI_ICD.csv'
-# )
 
 class QueryFromJson:
     def __init__(
@@ -409,11 +364,4 @@ class QueryFromJson:
 
         return qry_out 
 
-# vs_obcmi = QueryFromJson(
-#     url = 'https://raw.githubusercontent.com/RWD2E/phecdm/refs/heads/main/res/valueset_curated/vs-comorb-OBCMI.json',
-#     cd_field = 'DX',
-#     cdtype_field = 'DX_TYPE'
-# )
-# vs_obcmi.gen_cdtype_encoder()
-# print(vs_obcmi.gen_qry())
 
